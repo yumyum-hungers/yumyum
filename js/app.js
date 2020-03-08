2b641ce2006cbce9c697c99f5ec3e8327de2e596
X-Anas-X <anas.zain1996@gmail.com>
2020-03-08 21:46:52 +0200

    added js

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ var usernum;
 // to locate current company
 var companynum;
 // this contructor  companies
-var Companies = function() {
+var Companies = function(companies) {
   // this array contain the copmany object so this is the main that we will use and store , when you take the data from local sorage you should put it here .
-  this.company = [];
+  this.company = companies;
 };
 
 
@@ -19,25 +19,27 @@ Companies.prototype.addCompany = function(company, logo) {
 Companies.prototype.saveToLocalStorage = function() {
   // we call this method to store data on local storage
   localStorage.companies = JSON.stringify(this.company);
+  localStorage.usernum = usernum;
+  localStorage.companynum = companynum;
 };
 
 var Company = function(company, logo) {
   //thiss is company fconsructor
   this.companyName = company;
   this.logoDirectory = logo;
-  this.user = [];
+  this.users = [];
   this.restorant = [];
 };
 
-Company.prototype.addUser = function(name,password,number,breakHour,visaType,visaNumber) {
+Company.prototype.addUser = function(name,password,number,email,breakHour,employeesNumber,visaNumber) {
   // here we add user to company with all data needed and add it to user array ,so when ever we want to add user to company we should call this method
-  var thisUser = new Users(name,password,number,breakHour,visaType,visaNumber);
-  this.user.push(thisUser );
+  var thisUser = new Users(name,password,number,email,breakHour,employeesNumber,visaNumber);
+  this.users.push(thisUser );
 };
 
-Company.prototype.addRestorants = function(name,password,number,breakHour,visaType,visaNumber) {
+Company.prototype.addRestorants = function(name,logo) {
   // here we add restorant to company usually we will fill it in the code for now
-  var thisRestorant = new Restorant(name,password,number,breakHour,visaType,visaNumber);
+  var thisRestorant = new Restorant(name,logo);
   this.restorant.push(thisRestorant );
 };
 var Users = function(name,password,number,email,breakHour,employeesNumber,visaNumber) {
@@ -53,13 +55,14 @@ var Users = function(name,password,number,email,breakHour,employeesNumber,visaNu
 };
 
 
-var Restorant = function(name,logo,totalvoteses) {
+var Restorant = function(name,logo) {
   // to store data of every restorant
   this.name = name;
   this.logo = logo;
-  this.totalvoteses =totalvoteses;
+  this.totalvoteses ;
   this.order =[];
-  this.menu = [];
+  this.menu ;
+  this.menuImage ;
 };
 Restorant.prototype.votes = function() {
   //this methode we will use when click happen so we will add votes to restorant in company ,and add 1 to total votes
@@ -67,3 +70,35 @@ Restorant.prototype.votes = function() {
   this.order.push(Companies.company[companynum].user[usernum].name);
 
 };
+// how to load
+// function loadCompanies() {
+//     var companiesFromLocalStorage = JSON.parse(localStorage.getItem('companies')) || [];
+//     var companies = new Companies(companiesFromLocalStorage);
+//   }
+if(localStorage.getItem('companies') === null){
+  console.log('give it data');
+  var companyToAdd = ['LTUC','Arab Bank','Hikma','JU'];
+  var logoToAdd = ['images\LUCT-600x400.jpg','images\arabi.jpg','images\hikma.jpg','images\jordan-uni.png'];
+
+  var mc = new Restorant('McDonald\'s','');
+  mc.menu =['Big Mac Reagular Meal','Chicken McNuggets (6 pcs)','McChiken Regular Meal','Big Teasty Medium Meal'];
+  mc.menuImage = ['https://www.firefly-burgers.com/wp-content/uploads/2018/07/logo.png','','',''];
+
+  var pizzaHut = new Restorant('Pizza Hut','');
+  pizzaHut.menu =['HAWAIIAN','HOT STUFF BEEF','MEAT LOVERS','PEPPERONI'];
+  pizzaHut.menuImage = ['','','',''];
+
+  var mrPotato = new Restorant('Mr Potato','');
+  mrPotato.menu =['Crispy Baked Potato Wedges','Potato With Chees','Potato without Potato ','Capmer\'s Potato'];
+  mrPotato.menuImage = ['','','',''];
+
+  var companies = new Companies([]);
+  for (var i in companyToAdd) {
+    companies.addCompany(companyToAdd[i],logoToAdd[i]);
+    companies.company[i].restorant = [mc,pizzaHut,mrPotato];
+  }
+  localStorage.companies = JSON.stringify(companies.company);
+
+}else{
+  console.log('do nothing');
+}
