d1ece57145652d385d552e78d0a2ce66e6abf950
rowidsayyed <rowidsayyed@gmail.com>
2020-03-13 16:00:40 +0200

    fix bugs

--- js/user.js
+++ js/user.js
@@ -44,8 +44,8 @@ votesCalc();
 
 resturants();
 
-// Declare event listeners for every restorant
-listenersfun();
+// // Declare event listeners for every restorant
+// listenersfun();
 
 /******************************************************************************************************** */
 
@@ -98,6 +98,11 @@ function votesCalc(){
 
 function resturants(){
   var main = document.getElementById('resturantsName');
+  main.innerHTML = '';
+  var previousButton = document.createElement('div');
+  previousButton.id = 'previous';
+  previousButton.textContent = 'Previous Order';
+  main.appendChild(previousButton);
   var h2El =document.createElement('h5');
   h2El.innerText = 'Restaurants List ';
   main.appendChild(h2El);
@@ -108,15 +113,43 @@ function resturants(){
   divEl.className = 'allResturants';
   main.appendChild(divEl);
 
+  var offerButton = document.createElement('span');
+  divEl.appendChild(offerButton);
+  offerButton.className ='offerButton';
+  offerButton.textContent = 'Restaurants offers';
+  offerButton.addEventListener('click',offers);
+
   for (var res in companies.company[companyIndex].restorant) {
     var divresturant = document.createElement('span');
     divEl.appendChild(divresturant);
-    divresturant.innerHTML = `<div  class='resturant'  >
-        <img  id = res:${res} src= '${companies.company[companyIndex].restorant[res].logo}' /> </div>` ;
+    var divLogo = document.createElement('div');
+    divresturant.appendChild(divLogo);
+    divLogo.className = 'resturantLogo';
+    divLogo.id = `res:${res}`;
+    divLogo.innerHTML = `<div  class='resturant'  >
+        <img  id = imgres:${res} src= '${companies.company[companyIndex].restorant[res].logo}' /> </div>` ;
   }
+  // Declare event listeners for every restorant
+  listenersfun();
 }
 
 
+function offers(e){
+  if(e.target.textContent === 'Restaurant logos'){
+    resturants();
+  }else{
+    e.target.textContent = 'Restaurant logos';
+    var resturantLogoClass = document.querySelectorAll('.resturantLogo');
+    for(let i = 0 ; i < resturantLogoClass.length;i++ ){
+      var restdiv = document.getElementById(`res:${i}`);
+      let res = restdiv.id.split(':')[1] ;
+      resturantLogoClass[i].textContent =`Number of orders for 
+      ${companies.company[companyIndex].restorant[res].name} equall to ${companies.company[companyIndex].restorant[res].totalvoteses} and when ${companies.company[companyIndex].companyName} reach ${companies.company[companyIndex].restorant[res].offer} order you will take 10% discount`;
+      resturantLogoClass[i].style.background = 'lightgray';
+      resturantLogoClass[i].style.backgroundColor = 'rgba(0,0,0,0.05)';
+    }
+  }
+}
 
 function listenersfun(){
   for(let res =0 ; res < companies.company[companyIndex].restorant.length;res++){
@@ -185,13 +218,21 @@ function previousClick(){
 
 
 function resturantClick(e){
-  e.target.className ='wow slideOutUp';
-  new WOW().init();
-  changeContent(e);
+  var resturantLogoClass = document.querySelectorAll('.resturantLogo');
+  var res = e.target.id.split(':')[1];
+  for (let i = 0 ; i< resturantLogoClass.length;i++){
+    if(resturantLogoClass[i].id === `res:${res}`){
+      resturantLogoClass[i].className ='wow slideOutUp resturantLogo';
+      new WOW().init();
+      changeContent(e);
 
-  setTimeout(() => {
-    e.target.className ='';
-  }, 1000);
+      setTimeout(() => {
+        // e.target.className ='';
+        resturantLogoClass[i].className ='resturantLogo';
+      }, 1000);
+
+    }
+  }
 
 }
 
@@ -220,6 +261,14 @@ function changeContent(e){
   contentVariable.appendChild(resturantName);
   var res =e.target.id.split(':')[1];
   resturantName.textContent = companies.company[companyIndex].restorant[res].name;
+
+
+  var companyOrders = document.createElement('div');
+  contentVariable.appendChild(companyOrders);
+  companyOrders.id = 'companyOrders';
+  companyOrders.textContent = `# of Orders for ${companies.company[companyIndex].companyName}
+  is ${companies.company[companyIndex].restorant[res].totalvoteses}`;
+
   var resturantImg = document.createElement('img');
   resturantImg.id = 'resturantImages';
   contentVariable.appendChild(resturantImg);
@@ -246,7 +295,6 @@ function changeContent(e){
     menuMeal.textContent = '+';
     menuMeal.id = `res:${res}.mealImage:${meal}`;
     menuMeal.className ='mealButton';
-
   }
 
   let menuMeal = document.querySelectorAll('.meal');
@@ -351,6 +399,7 @@ function thanksfun(){
   for( let i in allcost){
     c += parseFloat(allcost[i]);
   }
+  allcost = [];
   var yourCost = document.createElement('h6');
   yourCost.textContent =`Your total cost will be ${c} JD`;
   yourCost.className = 'wow fadeInUp';
